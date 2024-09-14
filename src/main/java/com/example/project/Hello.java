2f24cc37d26d078c565b1b3492b082afed185a26
mohahuss27 <mohahuss27@gmail.com>
2024-09-14 09:39:18 +0300

    Update Hello.java

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -3,7 +3,7 @@ package com.example.project;
 public class Hello {
 
 	public static void main(final String[] args) {
-		System.out.println("Helloworld!");
+		System.out.println("Hello world!");
 	}
 
 }
